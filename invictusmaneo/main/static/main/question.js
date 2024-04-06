c2cc4d435e5ee7db9d1b9e6685ef9d3a7ae7b0a1
QtaZva <145173618+QtaZva@users.noreply.github.com>
2024-04-06 20:48:23 +0900

    Добавлена проверка на правильность выбранного ответа

--- invictusmaneo/main/static/main/question.js
+++ invictusmaneo/main/static/main/question.js
@@ -1,4 +1,3 @@
-
 var modal = document.getElementById('myModal');
 var btn = document.getElementById("myBTN");
 var answ = document.getElementById("asnBTN");
@@ -7,20 +6,14 @@ btn.onclick = function(){
     modal.style.display = "block";
 }
 
-answ.onclick = function(){
-    modal.style.display = "block";
-    modal.style.display = "none";
-    window.location.reload();
-}
-
 window.onclick = function(event){
     if (event.target == modal){
         modal.style.display = "none";
     }
 }
 
-const audio = document.getElementById('myAudio');
-const playButton = document.getElementById('myBTN');
+var audio = document.getElementById('myAudio');
+var playButton = document.getElementById('myBTN');
 
 playButton.addEventListener('click', function() {
   if (audio.paused) {
@@ -29,3 +22,68 @@ playButton.addEventListener('click', function() {
     audio.pause();
   }
 });
+
+var label1 = document.getElementById('answer');
+var label2 = document.getElementById('wrongAnswer1');
+var label3 = document.getElementById('wrongAnswer2');
+
+var rightAnswer = label1.innerHTML;
+
+let answersArr = [label1.innerHTML, label2.innerHTML, label3.innerHTML]
+
+function shuffleArray(array) {
+  for (var i = array.length - 1; i > 0; i--) {  
+      var j = Math.floor(Math.random() * (i + 1));
+                 
+      var temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+  }
+     
+  return array;
+}
+
+answersArr = shuffleArray(answersArr)
+
+alert(rightAnswer)
+
+label1.innerHTML = answersArr[0];
+label2.innerHTML = answersArr[1];
+label3.innerHTML = answersArr[2];
+
+var input1 = document.getElementById('input1');
+var input2 = document.getElementById('input2');
+var input3 = document.getElementById('input3');
+
+answ.onclick = function(){
+  if(input1.checked){
+    if(label1.innerHTML == rightAnswer){
+      alert('Правильно')
+    }
+    else{
+      alert('Неправильно')
+    }
+  }
+
+  if(input2.checked){
+    if(label2.innerHTML == rightAnswer){
+      alert('Правильно')
+    }
+    else{
+      alert('Неправильно')
+    }
+  }
+
+  if(input3.checked){
+    if(label3.innerHTML == rightAnswer){
+      alert('Правильно')
+    }
+    else{
+      alert('Неправильно')
+    }
+  }
+
+  modal.style.display = "block";
+  modal.style.display = "none";
+  window.location.reload();
+}
